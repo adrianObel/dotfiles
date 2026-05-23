77cc4d0517a82e696be7bfe3c904b1d9aeec6c43
Adrian Obelmejias <obeladrian@gmail.com>
2026-05-23 10:53:12 -0400

    feat(nvim): update fold icons

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -6,16 +6,14 @@ local o = vim.o
 -- Code folds
 o.foldmethod = "expr"
 o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-o.foldlevel = 99 -- open all folds by default
-o.foldnestmax = 4 -- don't fold beyond 4 levels deep
+o.foldlevel = 99
+o.foldnestmax = 4
 o.foldenable = true
 o.foldcolumn = "1"
 
 vim.opt.fillchars = {
   fold = " ",
-  foldopen  = "\u{F078}",  -- nf-fa-chevron_down
-  foldclose = "\u{F054}",  -- nf-fa-chevron_right
+  foldopen = "",
+  foldclose = "",
   foldsep   = " ",
-  diff      = "╱",
-  eob       = " ",
 }
